646d482126be500886fe4319c52465d08278a1b3
ahmedrimon <ahmedrimon@gmail.com>
2021-04-07 10:10:57 +0600

    new added

---
+++ src/components/PostDetail/PostDetail.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import Comment from '../Comment/Comment';
+
+const PostDetail = () => {
+    const {id} = useParams();
+    const [post, setPost]= useState({});
+    const [comments, setComments]= useState([]);
+
+    useEffect(() => {
+        const url = `https://jsonplaceholder.typicode.com/posts/${id}`
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setPost(data))
+    }, [id])
+
+    useEffect(() => {
+        const url = `https://jsonplaceholder.typicode.com/comments/${id}`
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setComments(data))
+    }, [id])
+    return (
+        <div>
+            <h2>This is postdetail: {id}</h2>
+            <p>User posted: {post.id}</p>
+            <p>Title: {post.title}</p>
+            <p>Body: {post.body}</p>
+            <p>Number of comments: {comments.length}</p>
+            {
+                comments.map(comment => <Comment comment={comment}></Comment>)
+            }
+        </div>
+    );
+};
+
+export default PostDetail;
